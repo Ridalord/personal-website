35776477059dbc47f19f4c698cabee374a6c3a5e
Ridalord <ridhwan.alabi@gmail.com>
2023-07-07 13:10:01 +0100

    Rendered portfolio projects from javascript file

--- script.js
+++ script.js
@@ -49,3 +49,74 @@ if (experienceWrap) {
     experienceWrap.appendChild(experienceItem)
   }
 }
+
+const portfolioItems = [
+  {
+    id: 'portfolio1',
+    title: 'React Portfolio',
+    img: 'img/Capture.PNG',
+    description: 'Discover the power of modern web development as you navigate through a collection of my projects. With a sleek design and seamless interactivity, this portfolio showcases my expertise in building engaging and responsive web experiences.',
+    stack: 'HTML, CSS, React, Bootstrap',
+    preview: 'https://ridalord.github.io/react-portfolio/',
+    code: 'https://github.com/Ridalord/react-portfolio'
+  },
+  {
+    id: 'businessId',
+    title: 'My Buisness ID',
+    img: 'img/portfolio2.png',
+    description: "Welcome to my personal website, where I showcase my skills, experience, and interests as a frontend developer. Explore my expertise in HTML5, CSS, JavaScript, React.js, and SEO, along with my personal interests. Connect with me on social media.",
+    stack: 'HTML, CSS, React',
+    preview: 'https://ridalord.github.io/rolleyDicey/',
+    code: 'https://github.com/Ridalord/rolleyDicey'
+  },
+  {
+    id: 'rolleyDicey',
+    title: 'Rolley Dicey',
+    img: 'img/rolleyDicey.png',
+    description: "Rolley Dicey is a dynamic and exciting dice game. It provides a user-friendly interface where two players compete to reach a target score by rolling the dice. With its intuitive design and engaging gameplay, Rolley Dicey offers an enjoyable experience for dice game enthusiasts.",
+    stack: 'HTML, CSS, JavaScript',
+    preview: 'https://ridalord.github.io/rolleyDicey/',
+    code: 'https://github.com/Ridalord/rolleyDicey'
+  },
+  {
+    id: 'lifestyle',
+    title: 'The 360 Lifestyle',
+    img: 'img/lifestyle.png',
+    description: "Experience a lifestyle website that offers a captivating blog and seamless affiliate marketing. Discover inspiring content and conveniently explore recommended products in one place.",
+    stack: 'HTML, CSS, JavaScript',
+    preview: 'https://ridalord.github.io/the_360_lifestyle/index.html',
+    code: 'https://github.com/Ridalord/the_360_lifestyle'
+  },
+  {
+    id: 'yhwhtech',
+    title: 'YHWH Tech',
+    img: 'img/yhwhtech.png',
+    description: "The stockbroker site is a user-friendly platform for seamless stock trading and investment. It provides real-time market data, advanced tools, and personalized features to help users make informed investment decisions and manage their portfolios effectively.",
+    stack: 'HTML, CSS, JavaScript, Bootstrap',
+    preview: 'https://ridalord.github.io/yhw-tech/',
+    code: 'https://github.com/Ridalord/yhw-tech'
+  }
+]
+const projectWrap = document.querySelector('.project-wrap')
+if (projectWrap) {
+  for (let i = 0; i < portfolioItems.length; i++){
+    const portfolioItem = document.createElement('div')
+    portfolioItem.classList.add('project-item')
+    portfolioItem.setAttribute('div', `${portfolioItems[i].id}`)
+    portfolioItem.innerHTML = `
+      <div class="project-img">
+        <img src="${portfolioItems[i].img}" alt="">
+      </div>
+      <div class="project-details">
+        <h3>${portfolioItems[i].title}</h3>
+        <p>${portfolioItems[i].description}</p>
+        <p class="project-stack"><span>Tech stack:</span> ${portfolioItems[i].stack}</p>
+        <div class="links d-flex">
+          <a href="${portfolioItems[i].preview}" target="_blank"><img src="img/akar-icons_link-chain.svg" alt="" srcset="">Live Preview</a>
+          <a href="${portfolioItems[i].code}" target="_blank"><img src="img/akar-icons_github-fill.svg" alt="" srcset="">View Code</a>
+        </div>
+      </div>
+    `
+    projectWrap.appendChild(portfolioItem)
+  }
+}
